b6aec4e8c65fcacac44acaddd4d78f1d07f98774
execMethod <neighborcell+github@gmail.com>
2019-09-06 18:45:10 +0900

    プロジェクト作成

---
+++ app/src/main/java/com/execmethod/azurewrath/MainActivity.java
@@ -0,0 +1,14 @@
+package com.execmethod.azurewrath;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
